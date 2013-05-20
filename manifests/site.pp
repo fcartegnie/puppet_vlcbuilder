7347f80c5964795f9d0d31e95aa8706ba06bf208
Francois Cartegnie <fcartegnie@free.fr>
2013-05-20 18:28:51 +0200

    move all resources to vlc class

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,19 +1,10 @@
 node default {
 
   if $hardwaremodel == "x86_64" and $operatingsystem == "Debian" {
-    class { 'mingw64': }
 
-    class { 'vlc::builddeps::win32': }
-
-    class { 'vlc::clone':
-      shallow => true
+    class { 'vlc':
+      target => 'win32'
     }
-
-    class { 'vlc::builder::win32':
-      require => [Class["vlc::clone"], Class["mingw64"], Class["vlc::builddeps::win32"]]
-    }
-
-    class { 'vlc::deploy::win': }
 
   } else {
     fail("Unsupported operating system or arch")
--- modules/vlc/manifests/init.pp
+++ modules/vlc/manifests/init.pp
@@ -1,4 +1,19 @@
 
-class vlc inherits vlc::params {
+class vlc ($target) inherits vlc::params {
+
+  case $target {
+    'win32' : {
+      class { 'mingw64': }
+
+      class { 'vlc::builddeps::win32': }
+
+      class { 'vlc::clone': shallow => true }
+
+      class { 'vlc::builder::win32': require => [Class["vlc::clone"], Class["mingw64"], Class["vlc::builddeps::win32"]] }
+
+      class { 'vlc::deploy::win': }
+    }
+    default: { notice("missing target") }
+  }
 
 }
