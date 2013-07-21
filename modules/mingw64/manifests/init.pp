81551623ce96f088ac707aa4519d06481d41f050
Francois Cartegnie <fcartegnie@free.fr>
2013-07-21 15:20:16 +0200

    downgrade extra repo priority

--- modules/mingw64/manifests/init.pp
+++ modules/mingw64/manifests/init.pp
@@ -20,7 +20,7 @@ class mingw64 {
     required_packages => "debian-archive-keyring",
     key               => "46925553",
     key_server        => "subkeys.pgp.net",
-    pin               => "1000",
+    pin               => "510",
     include_src       => false,
   }
 
